e46a800ffe970227efdd90cff4d8feb27e73d565
leozinfpss <88812048+leozinfpss@users.noreply.github.com>
2026-03-18 13:36:45 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+echo "PORT: $PORT"
+which nginx
+nginx -v
 php-fpm -D
 sleep 2
+envsubst '$PORT' < /etc/nginx/http.d/default.conf > /tmp/nginx.conf
+cp /tmp/nginx.conf /etc/nginx/http.d/default.conf
+nginx -t
 nginx -g "daemon off;"
